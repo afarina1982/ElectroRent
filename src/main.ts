bb218a3e1962f359b5b0844de84f45400b7f705e
Andres Fariña <farinavaldivia@gmail.com>
2024-10-30 12:07:16 -0300

    Swagger y Eliminacion .spec

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { version } from 'os';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const config = new DocumentBuilder()
+  .setTitle('API')
+  .setDescription('description')
+  .setVersion('0.0.1')
+  .addTag('ElectroRent')
+  .build();
+
+const document = SwaggerModule.createDocument(app, config);
+SwaggerModule.setup('api', app, document, {yamlDocumentUrl: 'swagger/yaml',});
+  
+await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
